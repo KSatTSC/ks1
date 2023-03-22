b80f3b0de5ea8f5f7549ce9e49552db8a6d815cc
KSatTSC <kushagra.srivastava@thesmartcube.com>
2023-03-22 06:25:18 +0000

    trying something

---
+++ eod.py
@@ -0,0 +1,11 @@
+list=[3,6,8,4,11,17,21,27,35,48]
+liste=[]
+listo=[]
+for i in list:
+    if (i%2==0):
+        liste.append(i)
+    else:
+        listo.append(i)
+print(liste)
+print(listo)
+        
